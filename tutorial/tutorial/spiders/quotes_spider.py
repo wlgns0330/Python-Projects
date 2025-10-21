e7eacac3bdaca5f49231aebca0ff9767488e5ee3
Hyun Lee <wlgns0330@gmail.com>
2025-10-21 11:10:36 -0600

    Scrapy Quotes and Author

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -7,7 +7,6 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         "https://quotes.toscrape.com/page/1/",
-        "https://quotes.toscrape.com/page/2/",
     ]
 
     def parse(self, response):
@@ -15,5 +14,10 @@ class QuotesSpider(scrapy.Spider):
             yield {
                 "text": quote.css("span.text::text").get(),
                 "author": quote.css("small.author::text").get(),
-                "tags": quote.css("div.tags a.tag::text").getall(),
+                "tags": quote.css("div.tags a.tag::text").getall()
             }
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
