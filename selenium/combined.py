ff7c248c550f85b7903472d5d83991f239f2b8a1
Hyun Lee <wlgns0330@gmail.com>
2025-12-11 19:55:29 -0700

    Test 2

--- selenium/combined.py
+++ selenium/combined.py
@@ -3,7 +3,7 @@ from graph import graph
 import sentiment_analysis
 
 # Website must be the link to the first episode of the webtoon to scrape
-website = "https://www.webtoons.com/en/romance/seasons-of-lovesome/episode-1/viewer?title_no=6881&episode_no=1"
+website = "https://www.webtoons.com/en/romance/the-grim-reaper-is-my-guardian/ep-0-prologue/viewer?title_no=8861&episode_no=1"
 info = website.split("/")
 title = info[5]
 
